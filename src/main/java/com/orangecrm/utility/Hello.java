d91bba3cc6f383b8d36180585107495a4c540f1b
SoftwareTestingByRajTripathi <tripathideva@gmail.com>
2023-05-27 13:36:34 +0530

    Update

---
+++ src/main/java/com/orangecrm/utility/Hello.java
@@ -0,0 +1,9 @@
+package com.orangecrm.utility;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello Selenium");
+		System.out.println("Welcome to Automation Testing");
+	}
+
+}
